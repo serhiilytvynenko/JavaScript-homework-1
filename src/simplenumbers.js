21d24326dd256ef5078474ff5b76ab1b16188729
serhii <serezhalytvynenko@gmail.com>
2023-05-03 14:24:26 +0200

    init

---
+++ src/simplenumbers.js
@@ -0,0 +1,15 @@
+'use srtict';
+for (let i = 2; i <= 100; i++) {
+  let simpleNumber = true;
+
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) {
+      simpleNumber = false;
+      break;
+    }
+  }
+
+  if (simpleNumber) {
+    console.log(i);
+  }
+}
